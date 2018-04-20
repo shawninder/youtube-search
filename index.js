9ee03719332dd902d83fef6b46804b4029b8470b
Shawn Inder <shawninder@gmail.com>
2018-04-19 20:00:37 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const url = require('url')
+const fetch = require('node-fetch')
+const qs = require('qs')
+
+module.exports = async (req, res) => {
+  const uri = url.parse(req.url)
+  const query = qs.parse(uri.query)
+  if (query.q) {
+    const data = await fetch(`https://www.googleapis.com/youtube/v3/search?${qs.stringify({
+      maxResults: '25',
+      part: 'snippet',
+      q: query.q,
+      type: '',
+      key: process.env.YOUTUBE_KEY
+    })}`)
+    data.body.pipe(res)
+  } else {
+    res.statusCode = 400
+    res.end()
+  }
+}
