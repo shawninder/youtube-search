da4072511ed3e691435573cc89c83b38a8c587a9
Shawn Inder <shawninder@gmail.com>
2018-09-16 13:24:33 -0400

    nothing

--- index.js
+++ index.js
@@ -6,14 +6,18 @@ module.exports = async (req, res) => {
   const uri = url.parse(req.url)
   const query = qs.parse(uri.query)
   if (query.q) {
-    const data = await fetch(`https://www.googleapis.com/youtube/v3/search?${qs.stringify({
+    const href = `https://www.googleapis.com/youtube/v3/search?${qs.stringify({
       maxResults: '25',
       part: 'snippet',
       q: query.q,
       pageToken: query.pageToken,
       type: '',
-      key: process.env.YOUTUBE_KEY
-    })}`)
+      key: process.env.YOUTUBE_KEY,
+      // Get only embeddable results
+      format: 5
+    })}`
+    // console.log('href', href)
+    const data = await fetch(href)
     // TODO use a specific origin here instead
     res.setHeader('Access-Control-Allow-Origin', '*')
     data.body.pipe(res)
